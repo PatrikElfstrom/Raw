881b3db9860c8085ef7aaf37bb39d230703b15f9
Patrik Elfström <me@patrikelfstrom.se>
2014-08-24 02:43:45 +0200

    First commit

---
+++ core.php
@@ -0,0 +1,128 @@
+<?php
+
+class Raw_Core {
+
+    var $uploaded_file_ending;
+	
+	function __construct() {
+    
+        // Check that the uploaded file is not a special file
+        add_filter('wp_handle_upload_prefilter', array( $this, 'check_file_type' ), 1, 1);
+        
+        // Add raw mimes to allowed mimes
+        add_filter('upload_mimes', array( $this, 'add_raw_mimes' ), 1, 1);
+        
+        // Override wordpress mime to exts function
+        add_filter('getimagesize_mimes_to_exts', array( $this, 'mime_to_exts_override' ), 1, 1);
+        
+        // Generate a thumbnail from the raw file and send it to wordpress
+        add_filter('wp_generate_attachment_metadata', array($this, 'onGenerateAttachmentMetadata'), 10, 2);
+	}
+    
+    // if the uploaded file ending was not in the special cases array
+    // remove the mime_to_exts_override filter (so it doesn't fuck up other files)
+    function check_file_type($file) {
+        if(isset($file['name'])) {
+            global $uploaded_file_ending;
+            $file_name_array = explode( '.', $file['name'] );
+            $file_ending = end( $file_name_array );
+            $uploaded_file_ending = $file_ending;
+            
+            $raw_file_types_special = raw()->settings['raw_file_types_special'];
+        
+            $raw_file_types_special = apply_filters( 'upload_mimes_raw_file_types_special', $raw_file_types_special );
+        
+            if(!in_array($file_ending, array_keys($raw_file_types_special))) {
+                remove_filter( 'getimagesize_mimes_to_exts', array( $this, 'replace_tiff_ext' ), 1 );
+            }
+        }
+        
+        return $file;
+    }  
+        
+    // Add raw files so WordPress accepts them.
+	public function add_raw_mimes($mimes) {
+        
+        $raw_file_types = raw()->settings['raw_file_types'];
+        
+        $raw_file_types = apply_filters( 'upload_mimes_raw_file_types', $raw_file_types );
+        
+        foreach($raw_file_types as $file_ending => $mime) {
+            $mimes[$file_ending] = $mime;
+        }
+        
+        return $mimes;
+	}
+    
+    // Wordpress checks the mime type (with getimagesize()) and sets the file ending to that mime type
+    // with files like DNG the mime type is TIFF but the file ending is DNG
+    // So wordpress would with this file replace the DNG file ending with TIF
+	public function mime_to_exts_override($mimes) {
+        $raw_file_types_special = raw()->settings['raw_file_types_special'];
+        global $uploaded_file_ending;
+        
+        $raw_file_types_special = apply_filters( 'upload_mimes_raw_file_types_special', $raw_file_types_special );
+        
+        foreach($raw_file_types_special as $file_ending => $mime) {
+        
+            // we do this check since both dng and cr2 has a tiff mime
+            if($file_ending == $uploaded_file_ending) {
+                $mimes[$mime] = $file_ending;
+            }
+        }
+        
+        return $mimes;
+	}
+    
+	public function onGenerateAttachmentMetadata($metadata, $attachment_id) {
+
+        // only generate thumbnail if raw and the attachment has no data
+        // this is to prevent a infinite loop
+        if ($this->isRaw($attachment_id) && empty($metadata)) {
+	        $attachment = get_post( $attachment_id );
+        	$attachment_path = get_attached_file($attachment_id);
+	        $thumbnail_filename = $attachment->post_title . '.jpg';
+
+            $thumbnail_blob = $this->generateThumbnail($attachment_id);
+        	$thumbnail = $this->uploadThumbnail($thumbnail_filename, $thumbnail_blob);
+
+	        if ($thumbnail['error'] === false) {
+        		$metadata = wp_generate_attachment_metadata($attachment_id, $thumbnail['file']);
+        		update_post_meta($attachment_id, '_original_path', $attachment_path);
+	        }
+        }
+
+        return $metadata;
+    }
+    
+    private function isRaw($attachment_id) {
+        
+        $raw_file_types = raw()->settings['raw_file_types'];
+        
+        $raw_file_types = apply_filters( 'upload_mimes_raw_file_types', $raw_file_types );
+        
+        return in_array(get_post_mime_type($attachment_id), $raw_file_types);
+    }
+    
+    private function generateThumbnail($attachment_id) {
+        $attachment_path = get_attached_file($attachment_id);
+        $filetype = wp_check_filetype($attachment_path);
+        
+        $image = new Imagick($filetype['ext'].':'.$attachment_path);
+        $image->setImageFormat('jpg');
+        $thumbnail_blob = $image->getImageBlob();
+        $image->clear();
+        $image->destroy();
+
+        return $thumbnail_blob;
+    }
+    
+    private function uploadThumbnail($filename, $bits) {
+        return wp_upload_bits($filename, null, $bits);
+    }
+}
+
+// initialize
+new Raw_Core();
+
+?>
---
+++ raw.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Raw
+Plugin URI: http://patrikelfstrom.se/raw
+Description: Raw enables you to upload RAW image files to WordPress with thumbnail support and without loosing or damaging the RAW file. Currently supporting DNG, CR2 and NEF. Filters allows you to add support for more file types.
+Version: 0.1
+Author: Patrik Elfström
+Author URI: http://patrikelfstrom.se/
+Copyright: Patrik Elfström
+Text Domain: raw
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if( ! class_exists('Raw') ) {
+
+    class Raw {
+        
+        // vars
+        var $settings;
+        
+        // A dummy constructor to ensure raw is only initialized once
+        function __construct() {
+            /* Do nothing here */
+        }
+        
+        
+        // Initialize: The real constructor to initialize raw
+        function initialize() {
+        
+            global $wp_settings_errors;
+            
+            // vars
+            $this->settings = array(
+                
+                // basic
+                'name'			=> __('Raw', 'raw'),
+                'version'		=> '0.1',
+                            
+                // urls
+                'basename'		=> plugin_basename( __FILE__ ),
+                'path'			=> plugin_dir_path( __FILE__ ),
+                'dir'			=> plugin_dir_url( __FILE__ ),
+                
+                // options
+                'raw_file_types'	=> array(
+                    'DNG' => 'image/AdobeRawDNG',
+                    'CR2' => 'image/x-canon-cr2',
+                    'NEF' => 'image/x-nikon-nef'
+                ),
+                
+                // This is to tell wordpress that just because the MIME is tiff
+                // doesn't mean that the file ending is tif
+                'raw_file_types_special' => array(
+                    'DNG' => 'image/tiff',
+                    'CR2' => 'image/tiff',
+                    'NEF' => 'image/tiff',
+                )
+            );
+                
+            // admin
+            if( is_admin() ) {
+        
+                // check that imagick is installed
+                if (!extension_loaded('imagick')) {
+                    
+                    add_action( 'admin_notices', function() {
+                        ?>
+                        <div class="error">
+                            <p><?php printf(__( 'The PHP module %sImageMagic%s is required for Raw to work.', 'raw' ), '<a target="_blank" href="http://php.net/manual/book.imagick.php">', '</a>');  ?></p>
+                        </div>
+                        <?php
+                    });
+                    
+                } else {
+            
+                    require_once('helpers.php');
+                    
+                    require_once('core.php');
+                }
+                
+            }
+            
+        }
+        
+    }
+
+    function raw() {
+
+        global $raw;
+        
+        if( !isset($raw) ) {
+        
+            $raw = new Raw();
+            
+            $raw->initialize();
+            
+        }
+        
+        return $raw;
+    }
+
+    // initialize
+    raw();
+
+}
+
+?>
